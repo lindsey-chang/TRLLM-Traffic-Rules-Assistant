31674aa7c71a42b493c5fc08b9af0d76559699d8
lindsey-chang <lindsey_chang@qq.com>
2024-02-01 12:04:50 +0800

    Generate llm_conversation_dataset_merge_v1.json

--- dataset/makellmdata.py
+++ dataset/makellmdata.py
@@ -1,14 +1,16 @@
-import json,re
+import json, re
+from script.file_opration import *
+
 """
 生成internML大模型训练所需的dataset格式
 """
 
-def merge_choose_answer_explanation(json_item):
 
-    choose=json_item.get("choose", "")
-    answer=json_item.get("answer", "")
+def merge_choose_answer_explanation(json_item):
+    choose = json_item.get("choose", "")
+    answer = json_item.get("answer", "")
     explanation = json_item.get("explanation", "")
-    choiceslist=extract_choiceslist_from_answer(choose,answer)
+    choiceslist = extract_choiceslist_from_answer(choose, answer)
 
     # 检查 choiceslist 是否为空
     if not choiceslist:
@@ -17,19 +19,17 @@ def merge_choose_answer_explanation(json_item):
         return "选项列表为空，无法合并。"
 
     merge_text = choiceslist[0].strip()
-    if len(choiceslist)>1:
-        for i in range(1,len(choiceslist)):
-            if i ==len(choiceslist)-1:
+    if len(choiceslist) > 1:
+        for i in range(1, len(choiceslist)):
+            if i == len(choiceslist) - 1:
                 merge_text += "和" + choiceslist[i]
                 break
-            merge_text+="、"+choiceslist[i]
+            merge_text += "、" + choiceslist[i]
 
-    merge_text+="。\n因为"
+    merge_text += "。\n因为"
     for exp in explanation:
-        merge_text+=exp
+        merge_text += exp
     return merge_text
-
-
 
 
 def extract_choiceslist_from_answer(choose, answer):
@@ -41,6 +41,7 @@ def extract_choiceslist_from_answer(choose, answer):
         if match:
             extracted_choices.append(match.group(1))
     return extracted_choices
+
 
 def create_json_object(system, input, output):
     """
@@ -63,24 +64,24 @@ def create_json_object(system, input, output):
 llm_json_structure = []
 # 读取 JSON 数据
 file_name = 'structqa_text.json'
-with open(file_name, 'r', encoding='utf-8') as file:
-    json_data = json.load(file)
+json_data=get_json_from_file(file_name)
 
-system="你现在是一名道路安全规则专家，你需要帮助用户解答各种交通规则问题以及向用户提供驾驶车辆需要了解的各种知识，你需要给出专业、可靠、有逻辑的回答，同时用词还需要具有亲和力。"
+system = "你现在是一名道路安全规则专家，你需要帮助用户解答各种交通规则问题以及向用户提供驾驶车辆需要了解的各种知识，你需要给出专业、可靠、有逻辑的回答，同时用词还需要具有亲和力。"
 for item in json_data:
-
-    input=item.get("question", "")
-    output=merge_choose_answer_explanation(item)
-    json_object=create_json_object(system,input,output)
+    input = item.get("question", "")
+    output = merge_choose_answer_explanation(item)
+    json_object = create_json_object(system, input, output)
     llm_json_structure.append({"conversation": [json_object]})
 
-# 将最终结构保存到新的 JSON 文件中
-output_file_name = 'llm_conversation_dataset_v1.json'
-with open(output_file_name, 'w', encoding='utf-8') as file:
-    json.dump(llm_json_structure, file, ensure_ascii=False, indent=4)
-
-
-
-
-
-
+llm_json_structure = llm_json_structure * 10  # 因原始数据集条数太少，故将数据集扩充10倍
+print(len(llm_json_structure))
+# # 将最终结构保存到新的 JSON 文件中
+# output_file_name = 'llm_conversation_dataset_v1.json'
+# with open(output_file_name, 'w', encoding='utf-8') as file:
+#     json.dump(llm_json_structure, file, ensure_ascii=False, indent=4)
+#
+# merge_and_save_json_files("llm_conversation_dataset_v1.json", "self_introduction.json",
+#                           "llm_conversation_dataset_merge_v1.json")
+file_name = 'llm_conversation_dataset_merge_v1.json'
+json_data_m=get_json_from_file(file_name)
+print(len(json_data_m))
--- script/file_opration.py
+++ script/file_opration.py
@@ -1,4 +1,27 @@
 import json
+
+def get_json_from_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+    return json_data
 def save_to_json_file(json_data, file_name):
     with open(file_name, 'w', encoding='utf-8') as file:
         json.dump(json_data, file, ensure_ascii=False, indent=4)
+
+def merge_and_save_json_files(file1_path, file2_path, output_file_path):
+    # 读取第一个 JSON 文件
+    with open(file1_path, 'r', encoding='utf-8') as file:
+        data1 = json.load(file)
+
+    # 读取第二个 JSON 文件
+    with open(file2_path, 'r', encoding='utf-8') as file:
+        data2 = json.load(file)
+
+    # 合并数据
+    merged_data = data1 + data2
+
+    # 保存合并后的数据到新的 JSON 文件
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        json.dump(merged_data, file, ensure_ascii=False, indent=4)
+
+    return merged_data
