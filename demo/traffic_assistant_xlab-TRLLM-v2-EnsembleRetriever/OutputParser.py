551e9389e67890852f1664a36df4e23c1443e24a
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-02-04 03:12:54 +0800

    Add files via upload

---
+++ demo/traffic_assistant_xlab-TRLLM-v2-EnsembleRetriever/OutputParser.py
@@ -0,0 +1,17 @@
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import List
+
+# Output parser will split the LLM result into a list of queries
+class LineList(BaseModel):
+    # "lines" is the key (attribute name) of the parsed output
+    lines: List[str] = Field(description="Lines of text")
+
+
+class LineListOutputParser(PydanticOutputParser):
+    def __init__(self) -> None:
+        super().__init__(pydantic_object=LineList)
+
+    def parse(self, text: str) -> LineList:
+        lines = text.strip().split("\n")
+        return LineList(lines=lines)
