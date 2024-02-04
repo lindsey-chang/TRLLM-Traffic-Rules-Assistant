ddb0ecb4e9afaafddee7e92707511c37372cbb67
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-02-04 15:15:07 +0800

    Update download_turbomind.py

--- download_turbomind.py
+++ download_turbomind.py
@@ -3,4 +3,4 @@
 # 下载InternLM-chat-7b模型
 import torch
 from modelscope import snapshot_download, AutoModel, AutoTokenizer
-model_dir = snapshot_download('heitao5200/TRLLM-Model-4bit', cache_dir='./model')
+model_dir = snapshot_download('heitao5200/TRLLM-Model-4bit_turbomind', cache_dir='./model')
