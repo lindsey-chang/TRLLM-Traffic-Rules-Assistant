8843cf3f8dc4330b3adbb7a0e85f3d175ca591e1
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-02-04 16:59:50 +0800

    Update download_turbomind.py

--- download_turbomind.py
+++ download_turbomind.py
@@ -1,10 +1,4 @@
-import os
 
-# 设置环境变量
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
-# 下载sentence-transformer模型
-os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir ./model/sentence-transformer')
 
 # 下载InternLM-chat-7b模型
 import torch
